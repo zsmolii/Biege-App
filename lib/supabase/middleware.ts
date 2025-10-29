06d998e0a20d9891683a7b7ef33c44b230ad121a
zsmolii <zsmolii@icloud.com>
2025-10-29 14:08:22 +0100

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -25,15 +25,8 @@ export async function updateSession(request: NextRequest) {
     },
   )
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  if (!user && !request.nextUrl.pathname.startsWith("/auth")) {
-    const url = request.nextUrl.clone()
-    url.pathname = "/auth/login"
-    return NextResponse.redirect(url)
-  }
+  // Session aktualisieren, aber nicht weiterleiten
+  await supabase.auth.getUser()
 
   return supabaseResponse
 }
