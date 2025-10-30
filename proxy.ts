7452306d6d2b86ff2cfb7d7aa9860fbaa8683592
zsmolii <zsmolii@icloud.com>
2025-10-30 21:08:30 +0100

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr"
 import { NextResponse, type NextRequest } from "next/server"
 
-export default async function proxy(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
